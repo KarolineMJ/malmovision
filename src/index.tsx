4efdad13be33fc53721cc01b9089e815b4ba0dc6
Karoline <karoline_mj@hotmail.com>
2023-05-13 00:10:48 +0200

    fix?

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import {
-  BrowserRouter,
-  RouterProvider,
-  createBrowserRouter,
-} from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import ErrorPage from './ErrorPage'
 import Quiz from './Quiz'
 import Bingo from './Bingo'
