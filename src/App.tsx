fc86690ca3557bcc718667d2715ba24966d621ed
Karoline <karoline_mj@hotmail.com>
2023-05-13 00:47:41 +0200

    remove complexity

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
+import Bingo from './Bingo'
 import Navbar from './navbar'
-import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Outlet />
+      <Bingo />
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,31 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom'
-import Quiz from './Quiz'
-import Bingo from './Bingo'
-import FAQ from './FAQ'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<Bingo />} />
-      <Route path="quiz" element={<Quiz />} />
-      <Route path="faq" element={<FAQ />} />
-    </Route>,
-  ),
-)
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
 )
 
 // If you want to start measuring performance in your app, pass a function
